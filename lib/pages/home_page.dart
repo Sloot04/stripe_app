e7c032d93a978bef08f5beed87bd0c6198a01261
Suyai Slootmans <suyaislootmans@gmail.com>
2021-09-08 12:14:45 -0300

    Monto a Pagar y boton de pago

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/credit_card_widget.dart';
 
 import 'package:stripe_app/data/tarjetas.dart';
+import 'package:stripe_app/widgets/total_pay_button.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -32,6 +33,10 @@ class HomePage extends StatelessWidget {
                         cvvCode: tarjeta.cvv,
                         showBackView: false);
                   }),
+            ),
+            Positioned(
+              bottom: 0,
+              child: TotalPayButton()
             )
           ],
         ));
